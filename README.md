dcf7bb29c680fd895e832abd31cfca5d8718fe4d
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-06-15 00:43:40 +0530

    add web server

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Minimalist Web Server
 a simple minimalist web server in C on Linux. made by following the tutorial  
 by Nir Lichman - [Making Minimalist Web Server in C on Linux | Nir Lichtman](https://www.youtube.com/watch?v=2HrYIl6GpYg)
+  
+compile the web server:
+```
+make web_server.c
+```
+run the web server:
+```
+./web_server
+```
+test the web server:
+```
+curl localhost:8000/FILE
+```
+replace FILE with an html file in the root directory.  
+  
+the web server exits after responding to one request. therefore, the server  
+has to be run everytime before sending in a request. 
+
+>[!WARNING]
+>it takes some time to free the port binded to the web server. therefore  
+>re-running the web server immediately will not work. wait for a little  
+>while before running the web server again.
+>
+>run the web server with `strace` to see if the port is active.
+>`strace ./web_server
---
+++ web_server.c
@@ -0,0 +1,53 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+const uint16_t PORT = 8000;
+
+void main() {
+    // create an endpoint for communication (fd: file descriptor)
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // bind a name (in address) to a socket
+    struct sockaddr_in address = {
+        AF_INET,
+        // port number in reversed byte order
+        htons(PORT),
+        0
+    };
+    bind(socket_fd, (struct sockaddr *) &address, sizeof(address));
+
+    // listen for connection on a socket
+    listen(socket_fd, 10);
+
+    // accept a connection on a socket
+    int client_socket_fd = accept(socket_fd, 0, 0);
+
+    // receive a message from socket (string from client)
+    char buffer[256] = {0};
+    recv(client_socket_fd, buffer, 256, 0);
+
+    // GET /index.html ... 
+    // extract 'index.html' from client request
+    char* requested_file = buffer + 5;
+    *strchr(requested_file, ' ') = 0;
+    
+    // open requested file
+    int opened_requested_file_fd = open(requested_file, O_RDONLY);
+
+    // transfer data between file descriptors
+    struct stat stat_buffer;
+    stat(requested_file, &stat_buffer);
+    sendfile(client_socket_fd, opened_requested_file_fd, 0, stat_buffer.st_size);
+
+    // close fd
+    close(opened_requested_file_fd);
+    close(client_socket_fd);
+    close(socket_fd);
+}
